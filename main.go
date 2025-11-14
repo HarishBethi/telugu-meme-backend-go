7c0175a7e77268195de45c6618f6416264e6731f
HarishBethi <harish.bethi123@gmail.com>
2025-11-14 15:13:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,11 +6,33 @@ import (
 )
 
 func main() {
+
+	// Home route
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		response := map[string]string{"message": "Go backend working!"}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(response)
 	})
 
+	// Templates API
+	http.HandleFunc("/templates", func(w http.ResponseWriter, r *http.Request) {
+
+		templates := []map[string]string{
+			{
+				"name": "Brahmi Krishna",
+				"url":  "https://res.cloudinary.com/dui1jtybs/image/upload/v1763113242/brahmi-krishna_ptqsqu.gif",
+			},
+			// Add more templates here as you upload them to Cloudinary
+			// {
+			//   "name": "Another Template",
+			//   "url":  "https://res.cloudinary.com/dui1jtybs/image/upload/.../template.jpg",
+			// },
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(templates)
+	})
+
+	// Start server on port 8080
 	http.ListenAndServe(":8080", nil)
 }
